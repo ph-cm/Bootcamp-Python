92e330ae69539403624bd0705a5c91c92f035197
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-06-24 18:39:27 -0300

    Statements

---
+++ Statements.py
@@ -0,0 +1,28 @@
+# #Expressions
+# Combination of f=values, variables, operators etc
+print(3 + 5)
+print("Hello" + " " + "World")
+
+age = 10
+print(age * 2)
+
+print(len("Python"))
+
+# #Statements
+# An instruction that Python can execute
+x = 10 #instruction to attribute value in a memory space
+print("Hello") #Intruction that print an "Hello" on the screen
+
+print("__________________________________________")
+#Exercicio: Quais das seguintes linhas sao expressoes e quais sao declaracoes
+resultado = 10 * 5
+"Python ROcks"
+
+x = 10
+if x > 0:
+    print("Blablabla")
+
+minha_lista = []
+item = 'batman'
+minha_lista.append(item)
+print(minha_lista)
