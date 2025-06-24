c5c2aee0d9e6b8aeabe2cde8acd9202f291e2c32
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-06-24 19:16:26 -0300

    Data Types

---
+++ DataType.py
@@ -0,0 +1,21 @@
+# Python has some data types that represents
+# diferents types of data. Its important to know because
+# this types determines whitch operations can be executed with that Value
+
+x = 10                  #int
+y = 3.14                #float
+z = "Python"            #str
+a = True                #boolean
+b = [1, 2, 3]           #list
+c = (4, 5)              #tuple
+d = {"Chave": "valor"}  #dictionary
+e = {1, 2, 3}           #set
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
