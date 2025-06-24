705996ce48a293ebb41ebbd0158cb24df6ca89dc
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-06-24 16:42:22 -0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#String = text
+name = "Pedro"
+#Integer = number int
+age = 22
+#Float = decimal
+height = 1.70
+#Boolean = true or false
+is_Studing = True
+
+#Printing values
+print(name)
+print(age)
+print(height)
+print(is_Studing)
+
