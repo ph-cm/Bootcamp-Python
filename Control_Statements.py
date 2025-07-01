21163e760203f143931bb7af3b0ff4d65ea3170f
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-07-01 11:09:36 -0300

    Control Statements (if, elif and else)

---
+++ Control_Statements.py
@@ -0,0 +1,23 @@
+# COndicionais
+# usadas para executar blocos de codigo diferentes com base em condicoes
+
+# if: executa o bloco se for true
+# elif: adiciona uma nova condicao
+# else: ultima alternativa
+
+idade = 18
+
+if idade >= 18:
+    print("Voce eh maior de idade")
+else:
+    print("Voce eh menor de idade")
+    
+temperatura = 28
+
+if temperatura > 30:
+    print("Esta quente")
+elif temperatura > 20:
+    print("Esta agradavel")
+else:
+    print("Esta frio")
+    
