966df1d93dccdcecde154650dd863455f3a9b982
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-07-15 11:27:07 -0300

    String

---
+++ leetcode/String.py
@@ -0,0 +1,10 @@
+# Sequencias imutaveis de caracteres. SImilar a arrays de caracteres, mas com restrição de imutabilidade.
+
+# Uso:
+#     -Anagramas, palindromos
+#     -Manipulação de texto, regex
+#     -Problemas de substring
+
+s = "hello"
+print(s[0])
+print(s[1:4])
